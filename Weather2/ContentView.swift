f35908b39effd1c341e3cc4895ada66b048b0d62
aa <aa>
2021-09-04 08:42:19 +0700

    Initial Commit

---
+++ Weather2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather2
+//
+//  Created by Loc Nguyen on 04/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather2/Weather2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather2App.swift
+//  Weather2
+//
+//  Created by Loc Nguyen on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
